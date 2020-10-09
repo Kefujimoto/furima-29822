ba4fff164c939206be260fd1f2e2bdbb1e3c3278
Kefujimoto <tengbenxian749@gmail.com>
2020-10-09 10:22:49 +0900

    ユーザー管理機能

--- Gemfile
+++ Gemfile
@@ -59,4 +59,10 @@ end
 
 gem 'devise'
 
-gem 'rspec-rails', '~> 4.0.0'
+gem 'rspec-rails'
+
+gem 'factory_bot_rails'
+  
+gem 'faker'
+
+gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :orders
+  has_many :items
+
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/ }
   validates :password_confirmation, presence: true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :kana_firstname, presence: true
-  validates :kana_lastname, presence: true
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :kana_firstname, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :kana_lastname, presence: true, format: { with: /\A[ァ-ン]+\z/ }
   validates :birthday, presence: true
 end
