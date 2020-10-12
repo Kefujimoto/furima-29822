2b3f7a38532b0edc11de435752ce54a92b431a2e
Kefujimoto <tengbenxian749@gmail.com>
2020-10-12 16:50:20 +0900

    first

--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :status, :charge, :area, :days
-
-  validates :category_id, :status_id, :charge_id, :area_id, :days_id, numericality: { other_than: 1 }
-
-end
