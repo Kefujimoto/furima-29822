1f3cacf14c996c625974436cc8b6ea609f562c9e
Kefujimoto <tengbenxian749@gmail.com>
2020-10-12 15:11:41 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,5 +17,5 @@ class Item < ApplicationRecord
   validates :charge_id, numericality: { other_than: 1 }
   validates :area_id, numericality: { other_than: 1 }
   validates :days_id, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: {greater_than: 300, less_than: 9999999}, format: { with: /\A[0-9]+\z/ } 
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ } 
 end
