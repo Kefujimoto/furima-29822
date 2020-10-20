4d270aff4cd8de6ed4825b1d5003cb4cf256ad86
Kefujimoto <tengbenxian749@gmail.com>
2020-10-20 16:20:46 +0900

    バリデーション修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
   validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
   validates :kana_firstname, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
